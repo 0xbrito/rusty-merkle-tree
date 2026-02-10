53b57017727873297b72ab6fb1b0af5885184654
Diego Brito <84880186+0xbrito@users.noreply.github.com>
2026-02-10 13:08:15 -0300

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,32 @@ use sha2::{Digest, Sha256};
 const ZERO_BYTES: [u8; 32] = [0; 32];
 
 pub struct MerkleTree {
-    leaves: Vec<[u8; 32]>,
+    pub leaves: Vec<[u8; 32]>,
 }
 
 impl MerkleTree {
-    pub fn new(leaves: Vec<[u8; 32]>) -> Self {
-        let leaves_capacity = leaves.len().next_power_of_two();
-        let mut hashes = leaves;
-        hashes.resize(leaves_capacity, ZERO_BYTES);
-        Self { leaves: hashes }
+    pub fn new() -> Self {
+        Self { leaves: Vec::new() }
+    }
+
+    pub fn from_slice(data: &[[u8; 32]]) -> Self {
+        let mut leaves = data.to_vec();
+        let cap: usize = leaves.len().next_power_of_two();
+
+        leaves.resize(cap, ZERO_BYTES);
+
+        Self { leaves }
     }
 
     fn compute_root(&self) -> [u8; 32] {
+        if self.leaves.is_empty() {
+            return ZERO_BYTES;
+        }
+
+        if self.leaves.len() == 1 {
+            return self.leaves[0];
+        }
+
         let mut hashes = self.leaves.clone();
         while hashes.len() > 1 {
             let pair_count: usize = hashes.len() / 2;
@@ -40,8 +54,49 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        // let res = MerkleTree(1, 2);
-        // assert_eq!(res, 3);
+    fn from_slice_empty() {
+        let tree = MerkleTree::from_slice(&[]);
+
+        assert_eq!(tree.leaves.len(), 1);
+        assert_eq!(tree.leaves[0], ZERO_BYTES);
+    }
+
+    #[test]
+    fn from_slice_single_leaf() {
+        let data = vec![[1; 32]];
+        let tree = MerkleTree::from_slice(&data);
+
+        assert_eq!(tree.leaves.len(), 1);
+        assert_eq!(tree.leaves[0], data[0]);
+    }
+
+    #[test]
+    fn from_slice_two_leaves() {
+        let data = vec![[1; 32], [2; 32]];
+        let tree = MerkleTree::from_slice(&data);
+
+        assert_eq!(tree.leaves.len(), 2);
+        assert_eq!(tree.leaves[0], data[0]);
+        assert_eq!(tree.leaves[1], data[1]);
+    }
+
+    #[test]
+    fn from_slice_three_leaves_pads_to_four() {
+        let data = [[1; 32], [2; 32], [3; 32]];
+        let tree = MerkleTree::from_slice(&data);
+
+        assert_eq!(tree.leaves.len(), 4);
+        assert_eq!(tree.leaves[3], ZERO_BYTES);
+    }
+
+    #[test]
+    fn from_slice_five_leaves_pads_to_eight() {
+        let data = [[1; 32], [2; 32], [3; 32], [4; 32], [5; 32]];
+        let tree = MerkleTree::from_slice(&data);
+
+        assert_eq!(tree.leaves.len(), 8);
+        assert_eq!(tree.leaves[5], ZERO_BYTES);
+        assert_eq!(tree.leaves[6], ZERO_BYTES);
+        assert_eq!(tree.leaves[7], ZERO_BYTES);
     }
 }
