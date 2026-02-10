a42464453d5f61cbce4bc969c8454d38935fb338
Diego Brito <84880186+0xbrito@users.noreply.github.com>
2026-02-10 00:54:57 -0300

    Implement MerkleTree with root computation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,38 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use sha2::{Digest, Sha256};
+
+const ZERO_BYTES: [u8; 32] = [0; 32];
+
+pub struct MerkleTree {
+    leaves: Vec<[u8; 32]>,
+}
+
+impl MerkleTree {
+    pub fn new(leaves: Vec<[u8; 32]>) -> Self {
+        let leaves_capacity = leaves.len().next_power_of_two();
+        let mut hashes = leaves;
+        hashes.resize(leaves_capacity, ZERO_BYTES);
+        Self { leaves: hashes }
+    }
+
+    fn compute_root(&self) -> [u8; 32] {
+        let mut hashes = self.leaves.clone();
+        while hashes.len() > 1 {
+            let pair_count: usize = hashes.len() / 2;
+            let mut next_level = Vec::with_capacity(pair_count);
+            for i in 0..pair_count {
+                let left = hashes[i * 2];
+                let right = hashes[i * 2 + 1];
+
+                let hash: [u8; 32] = Sha256::digest([left, right].concat())
+                    .as_slice()
+                    .try_into()
+                    .unwrap();
+                next_level.push(hash);
+            }
+            hashes = next_level;
+        }
+        hashes[0]
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +41,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let res = add(1, 2);
-        assert_eq!(res, 3);
+        // let res = MerkleTree(1, 2);
+        // assert_eq!(res, 3);
     }
 }
